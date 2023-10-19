92f2930ae78ea2f7b44eba9271228a5823b265b9
jruhterIllinois <105817581+jruhterIllinois@users.noreply.github.com>
2023-10-19 12:16:12 -0500

    Update and rename RTF2EMF (1).py to RTF2EMF.py

--- RTF2EMF.py
+++ RTF2EMF.py
@@ -1,3 +1,20 @@
+"""
+Python Script for converting a batch of .rtf files into .emf files
+
+No copyright is available. 
+
+For internal lab distribution only. 
+
+Author: Jeremy Ruhter (2023), University of Illinois Institute for Genomic Biology
+
+Usage: python NMS2TIFF.py
+
+Description: Iterate through the current directory looking for files with the .nms suffix 
+             and extracts the laser information and drops the topography. It will save a .tiff
+             file with the same name as the .nms
+
+"""
+
 
 import os
 import re
